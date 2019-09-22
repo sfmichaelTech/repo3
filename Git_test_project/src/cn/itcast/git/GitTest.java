3b50bedd26ee6eb41f413f35c3b61169af3684fc
michael <1466813761@qq.com>
2019-09-22 13:48:54 +0800

    提交到版本库

---
+++ Git_test_project/src/cn/itcast/git/GitTest.java
@@ -0,0 +1,13 @@
+package cn.itcast.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("这是一个git项目");
+        //hello test
+    }
+
+    public int add(int a , int b){
+        int i = a+b;
+        return a+b;
+    }
+}
---
+++ Git_test_project/src/cn/itcast/git/Second.java
@@ -0,0 +1,7 @@
+package cn.itcast.git;
+
+public class Second {
+    public void test(){
+        System.out.println("This is second Class");
+    }
+}
